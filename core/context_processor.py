390c3ac5deac7bca183470b94e5f3b63e79e2814
Bishnu <thapabishnu20@gmail.com>
2023-04-10 21:42:57 +0545

    fetching destinations based on their ordering number

--- core/context_processor.py
+++ core/context_processor.py
@@ -4,14 +4,22 @@ from django.shortcuts import render, get_object_or_404
 
 def default(request):
     destination = Destination.objects.all()
-    outbound = Destination.objects.exclude(name='Nepal')
+    first_dest = Destination.objects.get(ordering=1)
+    second_dest = Destination.objects.get(ordering=2)
+    third_dest = Destination.objects.get(ordering=3)
+    fourth_dest = Destination.objects.get(ordering=4)
+    outbound = Destination.objects.exclude(name=u'Nepal')
     activities = Tour_Type.objects.filter(destination__name=u'Nepal')
     company_info = CompanyInfo.objects.first()
     return {
         'destination': destination,
+        'first_dest': first_dest,
+        'second_dest': second_dest,
+        'third_dest': third_dest,
+        'fourth_dest': fourth_dest,
         'company_info': company_info,
         'activities': activities,
-        'outbound': outbound
+        'outbound': outbound,
     }
 
 
